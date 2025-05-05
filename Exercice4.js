1a3997b0aa7abd6759c649ade41e79ce45a74d50
Lagnien medhy <medhylagniencoulibaly@gmail.com>
2025-05-05 23:26:02 +0200

    Exercice 4 terminé

---
+++ Exercice4.js
@@ -0,0 +1,5 @@
+
+const base = prompt("Base du triangle : ");
+const hauteur = prompt("Hauteur du triangle : ");
+const aire = 0.5 * parseFloat(base) * parseFloat(hauteur);
+alert(`L'aire du triangle est : ${aire}`);
